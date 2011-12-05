ecf6d68bec36131dd5113893ad4d7657758d567f
U-EPC\evi <evi@EPC.(none)>
2011-12-06 03:15:46 +0500

    Task 3 corrections

--- task3.c
+++ task3.c
@@ -2,43 +2,54 @@
 
 int main(int argc, char* argv[]){
 	if(argc==1){
-		printf("%s\n","No args");
+		fprintf(stderr,"No args\n");
 		return 1;
 	}
-	unsigned char buffer[10000];
+	int n=1;
+	unsigned char buffer[512];
 	FILE* f=fopen(argv[1],"r");
 	if(f==NULL){
-		printf("%s\n","No file");
+		fprintf(stderr,"No file\n");
 		return 1;
 	}
-	int read=fread(buffer, 1, 10000, f);
-	if(read<10000){
-		printf("%s\n","Can't read, read");
-		return 1;
+	if(fseek(f,510,SEEK_SET)!=0){
+		fprintf(stderr,"Can't seek\n");
+		exit(1);
 	}
-	fclose(f);
-	int i;
-	unsigned char boot[512];
-	for(i=0;i<10000;++i){
-		if(buffer[i-1]==0x55&&buffer[i]==0xaa&&i>=511){
-			int j;
-			for(j=i-511;j<=i;++j){
-				boot[j-i+511]=buffer[j];
+	unsigned char prev=fgetc(f);
+	unsigned char c=fgetc(f);
+	while(feof(f)==0){
+		if(c==0xaa && prev==0x55){
+			if(fseek(f,-512,SEEK_CUR)!=0){
+				prev=c;
+				c=fgetc(f);
+				continue;
 			}
-			break;
+			int read=fread(buffer, sizeof(char), 512, f);
+			if(read < 512){
+				fseek(f, 512-read, SEEK_CUR);
+				prev=c;
+				c=fgetc(f);
+				continue;
+			}
+			char name[10];
+			sprintf(name, "boot%d", n);
+			FILE* boot=fopen(name, "w");
+			if(boot==NULL){
+				prev=c;
+				c=fgetc(f);
+				continue;
+			}
+			fwrite(buffer, sizeof(char), 512, boot);
+			fclose(boot);
+			char command[100];
+			sprintf(command, "ndisasm %s > %sdisasm",name,name);
+			system(command);
+			++n;
 		}
-	}
-	f=fopen("boot","w");
-	if(f==NULL){
-		printf("%s\n","Can't write bytes");
-		return 1;
-	}
-	int written=fwrite(boot,1,512,f);
-	if(written<512){
-		printf("%s\n","Not enough bytes written");
-		return 1;
+		prev=c;
+		c=fgetc(f);
 	}
 	fclose(f);
-	system("ndisasm boot>disasm");
 	return 0;
 }
